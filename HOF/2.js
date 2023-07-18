dfb3e65c493530a0856357dbb5e6fde12a593826
Vysakhdn <vysakhdn123@gmail.com>
2023-07-18 16:42:13 +0530

    commit

---
+++ HOF/2.js
@@ -0,0 +1,7 @@
+const a=[2,3,4,5,6]
+var b=a.map(function(e)
+{
+    return e*2
+})
+
+console.log(a,b)
---
+++ HOF/3.js
@@ -0,0 +1,8 @@
+const a=[2,3,4,5,6]
+var b=a.filter(function(e)
+{   
+
+        return e%3==0
+})
+
+console.log(a,b)
